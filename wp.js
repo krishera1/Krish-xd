6f0d2ceaac09c1ddca203d50e026d9921453ab2d
krishera1 <krisheraa2@gmail.com>
2024-11-02 13:27:17 +0545

    Update wp.js

--- wp.js
+++ wp.js
@@ -21,7 +21,7 @@
     const _0x3e09d7 = _0x1c864d => new Promise(_0x5da23c => _0x41d8de.question(_0x1c864d, _0x5da23c));
     const _0x1e9ef5 = () => {
       console.clear();
-      console.log("[1;32m\n __    __ _           _                         \n/ /\\ /\\ \\ |__   __ _| |_ ___  __ _ _ __  _ __  \n\\ \\/  \\/ / '_ \\ / _` | __/ __|/ _` | '_ \\| '_ \\ \n \\  /\\  /| | | | (_| | |\\__ \\ (_| | |_) | |_) |\n  \\/  \\/ |_| |_|\\__,_|\\__|___/\\__,_| .__/| .__/ \n                                   |_|   |_|    \n============================================\n[~] Author  : ABHI DON\n[~] GitHub  : KRISH DON ON TOP\n[~] Tool  : KRISH DON AUTOMATIC WHATSAPP MASSAGE SENDER\n============================================");
+      console.log("[1;32m\n __    __ _           _                         \n/ /\\ /\\ \\ |__   __ _| |_ ___  __ _ _ __  _ __  \n\\ \\/  \\/ / '_ \\ / _` | __/ __|/ _` | '_ \\| '_ \\ \n \\  /\\  /| | | | (_| | |\\__ \\ (_| | |_) | |_) |\n  \\/  \\/ |_| |_|\\__,_|\\__|___/\\__,_| .__/| .__/ \n                                   |_|   |_|    \n============================================\n[~] Author  : KRISH DON\n[~] GitHub  : KRISH DON ON TOP\n[~] Tool  : KRISH DON AUTOMATIC WHATSAPP MESSAGE SENDER\n============================================");
     };
     let _0x524dbd = [];
     let _0x4d8ae4 = [];
@@ -56,7 +56,7 @@
             }
             console.log("[1;32mTime => [0m" + _0x7cac94);
             console.log("[1;32mMessage => [0m" + _0x1f80a0);
-            console.log("    [ =============== KRISH DON MASSAGE SEND  =============== ]");
+            console.log("    [ =============== KRISH DON MESSAGE SEND  =============== ]");
             await _0x2bedd9(_0x1ad003 * 1000);
           } catch (_0x101498) {
             console.log("[1;33mError sending message: " + _0x101498.message + ". Retrying..." + "[0m");
